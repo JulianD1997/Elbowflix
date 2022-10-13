5bc77ee7d824ea4d8c1cf8700ce80b275504426c
Julian Casallas <julian.casallasb@gmail.com>
2022-10-13 00:11:20 -0300

    barra de navegacion

---
+++ scripts/nav_bar.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger')
+const navMenu = document.querySelector('.nav_menu')
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+document.querySelectorAll('.nav_link').forEach(n =>
+    n.addEventListener('click', () => {
+        burger.classList.remove('active')
+        navMenu.classList.remove('active')
+    })
+)
